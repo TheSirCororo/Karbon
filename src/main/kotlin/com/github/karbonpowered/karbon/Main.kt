1eb8d60555818202a38504887fbf6af9df15274e
XjCyan1de <xjcyan1de@yandex.ru>
2020-07-31 13:23:02 +0300

    WIP

--- src/main/kotlin/com/github/karbonpowered/karbon/Main.kt
+++ src/main/kotlin/com/github/karbonpowered/karbon/Main.kt
@@ -2,24 +2,33 @@ package com.github.karbonpowered.karbon
 
 import com.github.karbonpowered.api.Karbon
 import com.github.karbonpowered.api.catalog.CatalogKey
+import com.github.karbonpowered.api.item.inventory.ContainerTypes
+import com.github.karbonpowered.api.nbt.CompoundBinaryTag
+import com.github.karbonpowered.api.text.Text
+import com.github.karbonpowered.api.text.format.TextColor
 import com.github.karbonpowered.karbon.profile.KarbonGameProfileManager
 import com.github.karbonpowered.karbon.registry.KarbonBuilderRegistry
 import com.github.karbonpowered.karbon.registry.KarbonCatalogRegistry
 import com.github.karbonpowered.karbon.registry.KarbonFactoryRegistry
 import com.github.karbonpowered.protocol.java.MinecraftSession
 import com.github.karbonpowered.protocol.java.ProtocolState
+import com.github.karbonpowered.protocol.java.data.SlotData
+import com.github.karbonpowered.protocol.java.s2c.game.GameContainerItemsS2CPacket
 import com.github.karbonpowered.protocol.java.s2c.game.GameCustomPayloadS2CPacket
 import com.github.karbonpowered.protocol.java.s2c.game.GameKeepAliveS2CPacket
+import com.github.karbonpowered.protocol.java.s2c.game.GameOpenContainerS2CPacket
 import com.github.karbonpowered.text.translation.KarbonTranslationRegistry
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.awt.Color
 import java.net.InetSocketAddress
 import kotlin.random.Random
 
 
 fun main() {
     Karbon.initialize()
+    val empty = CompoundBinaryTag.empty()
     Server().apply {
         GlobalScope.launch {
             launch {
@@ -37,9 +46,16 @@ fun main() {
                 while (true) {
                     delay(100)
                     val brand = GameCustomPayloadS2CPacket(CatalogKey.minecraft("brand"), "§bKarbonPowered§f".toByteArray())
+                    val openContainer = GameOpenContainerS2CPacket(1, ContainerTypes.HOPPER, Text.of(TextColor.Companion.of(Color(13, 175, 16)), "My Menu"))
+                    val containerItems = GameContainerItemsS2CPacket(1, arrayOf(
+                            SlotData(1), SlotData(123, 5)
+                    ))
                     sessions.forEach {
-                        if (it is MinecraftSession && it.protocolState == ProtocolState.GAME)
+                        if (it is MinecraftSession && it.protocolState == ProtocolState.GAME) {
                             it.send(brand)
+                            it.send(openContainer)
+                            it.send(containerItems)
+                        }
                     }
                 }
             }
--- src/main/kotlin/com/github/karbonpowered/karbon/network/protocol/server/LoginServerProtocol.kt
+++ src/main/kotlin/com/github/karbonpowered/karbon/network/protocol/server/LoginServerProtocol.kt
@@ -5,6 +5,7 @@ import com.github.karbonpowered.api.entity.living.humanoid.player.gamemode.GameM
 import com.github.karbonpowered.api.nbt.CompoundBinaryTag
 import com.github.karbonpowered.api.nbt.ListBinaryTag
 import com.github.karbonpowered.api.nbt.set
+import com.github.karbonpowered.api.nbt.toBinaryTag
 import com.github.karbonpowered.api.profile.GameProfile
 import com.github.karbonpowered.protocol.java.ProtocolState
 import com.github.karbonpowered.protocol.java.c2s.login.LoginStartC2SPacket
@@ -28,7 +29,6 @@ object LoginServerProtocol : LoginProtocol(true) {
                     GameModes.SPECTATOR,
                     listOf(CatalogKey.minecraft("world")),
                     CompoundBinaryTag {
-//                        this["dimension"] = "yay"
                         this["dimension"] = ListBinaryTag {
                             add(CompoundBinaryTag {
                                 this["name"] = CatalogKey.minecraft("the_end")
@@ -62,4 +62,50 @@ object LoginServerProtocol : LoginProtocol(true) {
             session.send(GamePlayerPositionRotationS2CPacket(0.0, 100.0, 0.0, 0f, 0f, 0))
         }
     }
+}
+
+private fun a() {
+    val old = CompoundBinaryTag {
+        this["dimension"] = ListBinaryTag {
+            add(CompoundBinaryTag {
+                this["name"] = CatalogKey.minecraft("the_end")
+                this["natural"] = true
+                this["natural"] = true
+                this["ambient_light"] = 1.0f
+                this["has_ceiling"] = false
+                this["has_skylight"] = true
+                this["fixed_time"] = 0L
+                this["shrunk"] = false
+                this["ultrawarm"] = false
+                this["has_raids"] = false
+                this["respawn_anchor_works"] = false
+                this["bed_works"] = false
+                this["piglin_safe"] = false
+                this["logical_height"] = 256
+                this["infiniburn"] = CatalogKey.minecraft("netherrack")
+            })
+        }
+    }
+
+    val new = CompoundBinaryTag(
+            "dimension" to ListBinaryTag(
+                    CompoundBinaryTag(
+                            "name" to CatalogKey.minecraft("the_end").toBinaryTag(),
+                            "natural" to true.toBinaryTag(),
+                            "natural" to true.toBinaryTag(),
+                            "ambient_light" to 1.0f.toBinaryTag(),
+                            "has_ceiling" to false.toBinaryTag(),
+                            "has_skylight" to true.toBinaryTag(),
+                            "fixed_time" to 0L.toBinaryTag(),
+                            "shrunk" to false.toBinaryTag(),
+                            "ultrawarm" to false.toBinaryTag(),
+                            "has_raids" to false.toBinaryTag(),
+                            "respawn_anchor_works" to false.toBinaryTag(),
+                            "bed_works" to false.toBinaryTag(),
+                            "piglin_safe" to false.toBinaryTag(),
+                            "logical_height" to 256.toBinaryTag(),
+                            "infiniburn" to CatalogKey.minecraft("netherrack").toBinaryTag()
+                    )
+            )
+    )
 }
