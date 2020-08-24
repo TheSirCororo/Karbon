8290d68bfd9c2aa28c1a08ef221b5cfb47616799
XjCyan1de <xjcyan1de@yandex.ru>
2020-08-24 22:07:14 +0300

    Restructure complete

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,3 +36,40 @@ allprojects {
         }
     }
 }
+
+val karbonPoweredCommons = project(absoluteProjectPath("KarbonPowered-Commons"))
+val karbonPoweredMath = project(absoluteProjectPath("KarbonPowered-Math"))
+val karbonPoweredNetwork = project(absoluteProjectPath("KarbonPowered-Network"))
+val karbonPoweredTranslation = project(absoluteProjectPath("KarbonPowered-Translation"))
+val karbonPoweredText = project(absoluteProjectPath("KarbonPowered-Text"))
+val karbonPoweredApi = project(absoluteProjectPath("KarbonPowered-API"))
+val karbonPoweredNbt = project(absoluteProjectPath("KarbonPowered-NBT"))
+val karbonPoweredProtocol = project(absoluteProjectPath("KarbonPowered-Protocol"))
+
+karbonPoweredApi.dependencies {
+    api(karbonPoweredCommons)
+    api(karbonPoweredMath)
+    api(karbonPoweredNetwork)
+    api(karbonPoweredTranslation)
+    api(karbonPoweredText)
+}
+
+karbonPoweredNbt.dependencies {
+    api(karbonPoweredApi)
+}
+
+karbonPoweredProtocol.allprojects {
+    dependencies {
+        api(karbonPoweredApi)
+        api(karbonPoweredNbt)
+    }
+}
+
+// add KarbonPowered-Commons to all
+subprojects {
+    if (this != karbonPoweredCommons) {
+        dependencies {
+            api(karbonPoweredCommons)
+        }
+    }
+}
